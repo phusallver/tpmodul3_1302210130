f03bdc8b0b5de100cfb1934c3753e93c764e1f03
phusallver <103794258+phusallver@users.noreply.github.com>
2023-03-12 16:10:00 +0700

    menambahkan implementasi dengan state-based construction

--- C#/tpmodul3_1302210130/Program.cs
+++ C#/tpmodul3_1302210130/Program.cs
@@ -20,10 +20,66 @@ internal class Program
             40273};
             return kodePos[(int)lurah];
         }
-        private static void Main(string[] args)
+        /*private static void Main(string[] args)
         {
             System.Console.WriteLine(getKodePos(Kelurahan.Kujangsari));
+        }*/
+    }
+    public enum DoorState {TERBUKA, TERKUNCI}
+    public enum Trigger {BukaPintu, TutupPintu}
+    class DoorMachine
+    {
+        class transition
+        {
+            public DoorState stateAwal;
+            public DoorState stateAkhir;
+            public Trigger trigger;
+
+            public transition(DoorState stateAwal, DoorState stateAkhir, Trigger trigger)
+            {
+                this.stateAwal = stateAwal;
+                this.stateAkhir = stateAkhir;
+                this.trigger = trigger;
+            }
         }
+        transition[] transitions =
+        {
+            new transition(DoorState.TERBUKA, DoorState.TERBUKA, Trigger.BukaPintu),
+            new transition(DoorState.TERBUKA, DoorState.TERKUNCI, Trigger.TutupPintu),
+            new transition(DoorState.TERKUNCI, DoorState.TERBUKA, Trigger.BukaPintu),
+            new transition(DoorState.TERKUNCI, DoorState.TERKUNCI, Trigger.TutupPintu)
+        };
+        public DoorState getNextState(DoorState prevState, Trigger trigger)
+        {
+            DoorState nextState = prevState;
+            for (int i = 0; i < transitions.Length; i++)
+            {
+                if (transitions[i].stateAwal == prevState && transitions[i].trigger == trigger)
+                    nextState = transitions[i].stateAkhir;
+            }
+            return nextState;  
+        }
+        public DoorState currentState = DoorState.TERKUNCI;
+        public void activateTrigger(Trigger trigger)
+        {
+            currentState = getNextState(currentState, trigger);
+            if (currentState == DoorState.TERKUNCI)
+            {
+                System.Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                System.Console.WriteLine("Pintu tidak terkunci");
+            }
+        }
+        private static void Main(string[] args)
+        {
+            DoorMachine pintu = new DoorMachine();
+            System.Console.WriteLine("Sekarang pintu "+pintu.currentState);
+            System.Console.WriteLine("Membuka pintu...");
+            pintu.activateTrigger(Trigger.BukaPintu);
+        }
+
     }
 }
 
